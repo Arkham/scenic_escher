c9b59f9922ba20261783f355b2badfa78244f12e
Ju Liu <ju@noredink.com>
2019-03-05 10:36:20 +0000

    Improve errors

--- lib/scenes/home.ex
+++ lib/scenes/home.ex
@@ -9,48 +9,45 @@ defmodule ScenicEscher.Scene.Home do
     Hello World!
   """
 
+  @graph Graph.build(font: :roboto, font_size: 24, theme: :light)
+         |> group(
+           fn g ->
+             box = %Box{
+               a: %Vector{x: 75.0, y: 75.0},
+               b: %Vector{x: 250.0, y: 0.0},
+               c: %Vector{x: 0.0, y: 250.0}
+             }
+
+             {width, height} = Box.dimensions(box)
+
+             shapes = [Letter.f()]
+
+             picture = Fitting.create_picture(shapes)
+
+             paths =
+               box
+               |> picture.()
+               |> Rendering.to_paths({width, height})
+               |> IO.inspect()
+
+             initial = g
+             # |> text(@note, fill: :black, translate: {20, 40})
+
+             paths
+             |> Enum.reduce(initial, fn {elem, options}, acc ->
+               acc
+               |> path(elem, options)
+             end)
+           end,
+           translate: {20, 20}
+         )
+
   # ============================================================================
   # setup
 
   # --------------------------------------------------------
   def init(_, _) do
-    graph = build_graph()
-    push_graph(graph)
-    {:ok, graph}
-  end
-
-  def build_graph do
-    Graph.build(font: :roboto, font_size: 24, theme: :light)
-    |> group(
-      fn g ->
-        box = %Box{
-          a: %Vector{x: 75.0, y: 75.0},
-          b: %Vector{x: 250.0, y: 0.0},
-          c: %Vector{x: 0.0, y: 250.0}
-        }
-
-        {width, height} = Box.dimensions(box)
-
-        shapes = [Letter.f()]
-
-        picture = Fitting.create_picture(shapes)
-
-        paths =
-          box
-          |> picture.()
-          |> Rendering.to_paths({width, height})
-          |> IO.inspect()
-
-        initial = g
-        # |> text(@note, fill: :black, translate: {20, 40})
-
-        paths
-        |> Enum.reduce(initial, fn {elem, options}, acc ->
-          acc
-          |> path(elem, options)
-        end)
-      end,
-      translate: {20, 20}
-    )
+    push_graph(@graph)
+    {:ok, @graph}
   end
 end
